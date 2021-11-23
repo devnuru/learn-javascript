362bf0741785e624ca30434005ed57a54c45e93f
Md Nuruzzaman Chowdhury <devnuruzzaman@gmail.com>
2021-11-23 14:49:03 +0600

    Initial commit for learning javascript

---
+++ app.js
@@ -0,0 +1,53 @@
+
+const hey = () => {
+  document.getElementById("hello").innerHTML = "Hello JavaScript"
+}
+
+const bulbOn = () => {
+  document.getElementById("myImage").src='./images/pic_bulbon.gif'
+} 
+
+
+const bulbOff = () => {
+  document.getElementById("myImage").src='./images/pic_bulboff.gif'
+} 
+
+// innerHTML
+
+document.getElementById("inner").innerHTML = 5 + 5;
+
+// document.write
+
+const dw = () => document.write(5 + 10);
+
+// window.alert
+
+window.alert('Hello');
+
+console.log(5+5);
+
+let x, y, z;
+x= 5;
+y= 6
+z = x + y 
+
+console.log(z);
+
+// var => Declares a variable
+//  let => Declares a block variable
+//  const => Declares a block constant 
+//  if 
+//  switch
+//  for
+//  function 
+//  return
+//  try 
+
+/*
+
+Multi Line comment
+*/
+
+
+
+
